0f73b8511b0eb2285bf6d849ec4aef5c0298c41a
blackeagle94 <blackeagle4894@gmail.com>
2021-01-24 00:56:26 +0200

    completed but how i do not know :)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import './App.css';
 import Job from './Job';
 import JobPagination from './JobPagination';
+import SearchForm from './SearchForm';
 import useFetchJobs from './useFetchJobs'
 
 
@@ -10,10 +11,23 @@ function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page)
-console.log(hasNextPage)
+
+  const handleParamChange = e => {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prev => {
+      return {
+        ...prev,
+        [param]: value
+      }
+    })
+  }
+
   return (
     <Container className="my-4">
     <h1 className="mb-4">Github Jobs</h1>
+    <SearchForm params={params} onParamChange={handleParamChange}/>
     <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
     {loading && <h1>Loading...</h1>}
     {error && <h1>Error. Try Refreshing.</h1>}
